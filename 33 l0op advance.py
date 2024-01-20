5a25918e3e84ac91a94472901d69220d6de54c5d
Irxamp <88538593+Irxamp@users.noreply.github.com>
2024-01-20 20:46:39 +0200

    Add files via upload

---
+++ 33 l0op advance.py
@@ -0,0 +1,22 @@
+Varrows=int(input("Enter the number of rows for Pyramid Pattern"))
+
+for j in range(Varrows):
+ for l in range(j+1):
+  print(l+1, end="")
+ print("\n")
+ 
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+ 
---
+++ 35 l0op advance.py
@@ -0,0 +1,22 @@
+Varrows=int(input("Enter the number of rows for Pyramid Pattern"))
+
+for j in range(Varrows, 0,-1):
+ for l in range(0,j):
+  print("*",end="")
+ print("\n")
+ 
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+ 
